984f044394bf485f7799dc8aef745f232996a70f
KyleLiuCat <kyle.liu.coffeecat@gmail.com>
2016-12-13 20:35:04 +0800

    Blimk the  7688Duo  LED13

---
+++ ID13Auto.ino
@@ -0,0 +1,17 @@
+/*
+  控制板上的 LED 每秒on/off一次
+  LED pin 13
+*/
+void setup() {
+  // put your setup code here, to run once:
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(2000);
+}
