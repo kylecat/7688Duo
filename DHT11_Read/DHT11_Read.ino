7a2274aa60cdb583e4b283687159e8117e716152
KyleLiuCat <kyle.liu.coffeecat@gmail.com>
2016-12-15 11:09:29 +0800

    DHT11 Read V1.0
    Read the DHT11 data 
    Blink LDE(D13) when read the data

---
+++ DHT11_Read/DHT11_Read.ino
@@ -0,0 +1,72 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+#include <DHT.h>
+
+#define DHTPIN 12 //PIN D12
+#define LED 13
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+/* 
+ *  Initialize DHT sensor.
+ *  Note that older versions of this library took an optional third parameter to tweak the timings for faster processors.
+ *  This parameter is no longer needed as the current DHT reading algorithm adjusts itself to work on faster procs.
+ */
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!!");
+  pinMode(LED, OUTPUT);
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(1000);
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(2000);
+  
+  
+  // Reading temperature or humidity takes about 250 milliseconds
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  // Serial.print(f);
+  // Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  // Serial.print(hif);
+  // Serial.print(" *F");
+  Serial.println("\r\n ==================================================");
+
+  delay(1500); // sensoring by 5 sec
+}
